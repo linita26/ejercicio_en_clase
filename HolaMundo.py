7a926c20c28ee7e03e726d8a692770f5f2e23cf8
lina Maria <linita26.jl@gmail.com>
2016-08-08 20:29:40 -0500

    creacion del proyecto Hola Mundo

---
+++ HolaMundo.py
@@ -0,0 +1,144 @@
+
+# coding: utf-8
+
+# In[5]:
+
+print 'Hola Mundo'
+
+
+# $$\sum_{p}{li}(xp)-log(2)$$
+
+# In[ ]:
+
+
+
+
+# # Hola Mundo en Python
+# hola mundo desde Python
+
+# In[6]:
+
+2+4*6/8.0-3
+
+
+# In[7]:
+
+3/2
+
+
+# In[8]:
+
+3.0/2.0
+
+
+# In[9]:
+
+3+5j
+
+
+# In[10]:
+
+(3+5j)-(1-1j)
+
+
+# In[11]:
+
+abs(3+5j)
+
+
+# In[12]:
+
+5**3
+
+
+# In[13]:
+
+_
+
+
+# In[14]:
+
+int(_)
+
+
+# In[15]:
+
+int(_8)
+
+
+# In[18]:
+
+float(_12)
+
+
+# In[19]:
+
+complex(_13)
+
+
+# In[20]:
+
+round(_11)
+
+
+# In[22]:
+
+max(8,10,20,30)
+
+
+# In[23]:
+
+num=10
+
+
+# In[24]:
+
+num
+
+
+# In[25]:
+
+x,y,z,a=1,2,3,4
+
+
+# In[26]:
+
+y
+
+
+# In[27]:
+
+x,y,z
+
+
+# In[28]:
+
+x,y,z,a=a,z,y,x
+
+
+# In[29]:
+
+x,y,z
+
+
+# In[32]:
+
+x=5
+y=8
+
+
+# In[33]:
+
+x,y
+
+
+# In[41]:
+
+x=5
+y=8 
+
+if x>y:
+    print ("E mayor es",x)
+else:
+    print ("El menor es",y)
+
